a18bf5e996d565718671062bc54185c0e4de3449
Kade <26305836+Kade-github@users.noreply.github.com>
2024-01-07 16:43:31 -0800

    forgot these

---
+++ Includes.h
@@ -0,0 +1,23 @@
+/*
+    Copyright (c) 2023-2024 Kade
+
+    Please read the LICENSE.md file for more information on licensing.
+*/
+
+#pragma once
+
+/* Header guard */
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <list>
+#include <string>
+#include <unordered_map>
+
+#include <glad/gl.h>
+#include <glfw/glfw3.h>
+
+#endif
