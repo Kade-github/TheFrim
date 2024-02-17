a5b16c89864a493416718cd9c1532aaee846afa8
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-17 15:30:24 -0800

    Format

--- src/Engine/Objects/Base/Cube.cpp
+++ src/Engine/Objects/Base/Cube.cpp
@@ -18,27 +18,21 @@ void Cube::Draw()
 	glm::vec2 src = texture->getSrc(0, 0);
 
 	// front face
-
 	Game::instance->render->AddQuad(position, glm::vec3(width, height, 0), 0, src);
 
 	// back face
-
 	Game::instance->render->AddQuad(glm::vec3(position.x, position.y, position.z + depth), glm::vec3(width, height, 0), 0, src);
 
 	// left face
-
 	Game::instance->render->AddQuad(position, glm::vec3(0, height, depth), 0, src);
 
 	// right face
-
 	Game::instance->render->AddQuad(glm::vec3(position.x + width, position.y, position.z), glm::vec3(0, height, depth), 0, src);
 
 	// top face
-
 	Game::instance->render->AddQuad(glm::vec3(position.x, position.y + height, position.z), glm::vec3(width, 0, 0), depth, src);
 
 	// bottom face
-
 	Game::instance->render->AddQuad(position, glm::vec3(width, 0, 0), depth, src);
 
 	Game::instance->render->Draw(texture, Game::instance->shader, camera);
