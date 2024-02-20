f4038331f24208f12a36b8605372b5956b6c5680
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-19 21:08:34 -0800

    It now works on linux

--- src/Game/WorldManager.cpp
+++ src/Game/WorldManager.cpp
@@ -1,18 +1,9 @@
 #include "WorldManager.h"
 #include <Game.h>
 #include <fstream>
-#include <zstr.hpp>
+#include "../zstr/src/zstr.hpp"
 
-#ifdef _WIN32
 #include <filesystem>
-#else
-#include <experimental/filesystem>
-using namespace Data;
-namespace std
-{
-	namespace filesystem = experimental::filesystem;
-}
-#endif
 
 WorldManager* WorldManager::instance = nullptr;
 
@@ -30,7 +21,6 @@ WorldManager::WorldManager(std::string worldPath, Texture* _tp, std::function<vo
 		CreateWorld();
 	else
 		LoadWorld();
-
 }
 
 void WorldManager::UploadChunks()
