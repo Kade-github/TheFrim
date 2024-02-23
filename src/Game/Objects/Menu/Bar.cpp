f7e998cd56df5c9284c82d15d4ce83fc170b170e
Kade <26305836+Kade-github@users.noreply.github.com>
2024-02-23 11:39:35 -0800

    format and setup for bar

---
+++ src/Game/Objects/Menu/Bar.cpp
@@ -0,0 +1,6 @@
+#include "Bar.h"
+
+Bar::Bar(glm::vec3 _pos) : GameObject2D(_pos)
+{
+    
+}
---
+++ src/Game/Objects/Menu/Bar.h
@@ -0,0 +1,12 @@
+#ifndef _BAR_H
+#define _BAR_H
+
+#include <Objects/2DGameObject.h>
+
+class Bar : public GameObject2D
+{
+public:
+    Bar(glm::vec3 _pos);
+}
+
+#endif
