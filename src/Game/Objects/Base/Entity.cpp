8a761253222cad782bc70c7e072381b5d732e0ad
Kade <26305836+Kade-github@users.noreply.github.com>
2024-03-08 13:02:54 -0800

    almost back to where 0.0.2/1 were

--- src/Game/Objects/Base/Entity.cpp
+++ src/Game/Objects/Base/Entity.cpp
@@ -8,12 +8,159 @@ Entity::Entity(glm::vec3 pos) : GameObject(pos)
 
 void Entity::CheckCollision(glm::vec3& motion)
 {
-	// TODO: check collision with blocks
+Chunk* currentChunk = WorldManager::instance->GetChunk(motion.x, motion.z);
+
+	float toY = motion.y - 2;
+
+	glm::vec3 diff = motion - position;
+
+	glm::vec3 rp = position;
+
+	rp.y -= 2;
+
+	float sum = abs(diff.x) + abs(diff.y) + abs(diff.z);
+
+	if (sum == 0)
+		return;
+
+	if (currentChunk != NULL)
+	{
+		// raycast
+
+		float toX = motion.x;
+		float toZ = motion.z;
+
+		float pX = position.x;
+		float pZ = position.z;
+
+		glm::vec3 ray = position;
+		ray.y = toY;
+
+		bool hit = currentChunk->DoesBlockExist(toX, ray.y, toZ);
+
+		float progress = 0;
+
+		// first raycast (x axis)
+
+		float _lastX = motion.x;
+
+		while (progress < 1)
+		{
+			ray.x = rp.x + (diff.x * progress);
+
+			toX = (ray.x - currentChunk->position.x);
+
+			hit = currentChunk->DoesBlockExist(toX, ray.y, pZ);
+
+			if (hit)
+				break;
+
+			progress += 0.1;
+			_lastX = ray.x;
+		}
+
+		// second raycast (z axis)
+
+		progress = 0;
+
+		ray = position;
+		ray.y = toY;
+
+		float _lastZ = motion.z;
+
+		while (progress < 1)
+		{
+			ray.z = rp.z + (diff.z * progress);
+
+			toZ = (ray.z - currentChunk->position.z);
+
+			hit = currentChunk->DoesBlockExist(pX, ray.y, toZ);
+
+			if (hit)
+				break;
+
+			progress += 0.1;
+			_lastZ = ray.z;
+			
+		}
+
+		motion = glm::vec3(_lastX, position.y, _lastZ);
+	}
 }
 
 void Entity::CheckVerticalCollision(glm::vec3& motion)
 {
-	// TODO: check collision with blocks
+Chunk* currentChunk = WorldManager::instance->GetChunk(motion.x, motion.z);
+
+	isOnGround = false;
+
+	float toY = motion.y - 2;
+
+	if (downVelocity > 0)
+		toY = motion.y;
+
+	glm::vec3 rp = position;
+	rp.y -= 2;
+
+	glm::vec3 diff = motion - position;
+
+	float sum = abs(diff.x) + abs(diff.y) + abs(diff.z);
+
+	if (sum == 0)
+	{
+		return;
+	}
+
+	if (currentChunk != NULL)
+	{
+		// raycast
+		glm::vec3 ray = position;
+
+		ray.y = toY;
+
+		float toX = position.x;
+		float toZ = position.z;
+
+		int _lastY = -1;
+
+		bool hit = currentChunk->DoesBlockExist(toX, ray.y, toZ);
+
+		float progress = 0;
+
+		while (progress < 1)
+		{
+			ray.y = rp.y + (diff.y * progress);
+
+			hit = currentChunk->DoesBlockExist(toX, ray.y, toZ);
+
+			if (hit)
+			{
+
+				_lastY = (int)(ray.y);
+				break;
+			}
+
+			progress += 0.1;
+		}
+
+		if (_lastY >= 0)
+		{
+			if (downVelocity <= 0)
+			{
+				isOnGround = true;
+				downVelocity = 0;
+				motion.y = _lastY + 3;
+			}
+			else
+			{
+				if (_lastY > position.y)
+				{
+					motion.y = _lastY - 1;
+					downVelocity = 0;
+				}
+			}
+		}
+	}
 }
 
 bool Entity::RayTo(glm::vec3& to)
